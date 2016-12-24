037cf884016b2705c2353f983dd5f6575f0f7ab9
Avancha <Avancha@AvanchaSrinath>
2016-12-24 11:55:25 +0530

    Tested url working with welcome message
    Url is working
    
    Added Spring framework dependencies
    Added Tiles 
    dependencies to pom.xml
    xml configuration to dispatcher
    layout jsps
    
    Added Index controller
    Added dependencies for slf4j log4j

---
+++ src/main/java/com/srinathavan/mwb/controller/IndexController.java
@@ -0,0 +1,35 @@
+/**
+ *  
+ *   *  
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+package com.srinathavan.mwb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Avancha
+ *
+ */
+
+@Controller
+public class IndexController {
+
+	/**
+	 * 
+	 */
+	public IndexController() {
+		// TODO Auto-generated constructor stub
+	}	
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+}
